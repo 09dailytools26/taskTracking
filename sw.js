0cb0197b899bb627023adf01d9537752002ab40a
PIANO09.26 <gogohiromi1007@gmail.com>
2026-09-25 17:51:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Study Planner：オフラインでも開けるようにするための仕組み（記録はここには保存しません）
-const CACHE = "study-planner-v1";
-const FILES = ["./", "./index.html", "./guide.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+const CACHE = "study-planner-v2";
+const FILES = ["./", "./index.html", "./guide.html", "./guide-en.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 // 新しい版があればそれを使い、電波がないときは保存済みの版を表示
