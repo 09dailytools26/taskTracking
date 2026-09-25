970eddef558fac35e1115007214808d4c25474e3
PIANO09.26 <gogohiromi1007@gmail.com>
2026-09-25 17:31:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// 宿題手帳：オフラインでも開けるようにするための仕組み（記録はここには保存しません）
-const CACHE = "shukudai-techo-v5";
+// Study Planner：オフラインでも開けるようにするための仕組み（記録はここには保存しません）
+const CACHE = "study-planner-v1";
 const FILES = ["./", "./index.html", "./guide.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
